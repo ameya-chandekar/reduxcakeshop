d4f9b9bc804118992aec4e2e886dcae6179a6314
ameya <ameya.chandekar@neosoftech.com>
2020-02-28 08:34:02 +0530

    pretified code

--- src/redux/cake/CakeActions.js
+++ src/redux/cake/CakeActions.js
@@ -1,25 +1,17 @@
-import {BUY_CAKE} from'./caketypes'
-import {ADD_CAKE} from'./caketypes'
+import {BUY_CAKE} from './caketypes'
+import {ADD_CAKE} from './caketypes'
 import {BUY_ICECREAM} from './caketypes'
 import {ADD_ICECREAM} from './caketypes'
 
-export const buycake=()=>{
-    return{
-        type:BUY_CAKE
-    }
+export const buycake = () => {
+    return {type: BUY_CAKE}
 }
-export const addcake=()=>{
-    return{
-        type:ADD_CAKE
-    }
+export const addcake = () => {
+    return {type: ADD_CAKE}
 }
-export const buyicecream=()=>{
-    return{
-        type:BUY_ICECREAM
-    }
+export const buyicecream = () => {
+    return {type: BUY_ICECREAM}
 }
-export const addicecream=()=>{
-    return{
-        type:ADD_ICECREAM
-    }
+export const addicecream = () => {
+    return {type: ADD_ICECREAM}
 }
--- src/redux/cake/CakeReducer.js
+++ src/redux/cake/CakeReducer.js
@@ -1,29 +1,34 @@
-import {BUY_CAKE ,ADD_CAKE,BUY_ICECREAM, ADD_ICECREAM} from './caketypes'
+import {BUY_CAKE, ADD_CAKE, BUY_ICECREAM, ADD_ICECREAM} from './caketypes'
 
-const initialstate={
-    numOfCakes:10,
-    numOfIcecreams:20
-} 
+const initialstate = {
+    numOfCakes: 10,
+    numOfIcecreams: 20
+}
 
-const cakeReducer=(state=initialstate,action)=>{
-    switch(action.type){
-        case BUY_CAKE:return{
-            ...state,
-            numOfCakes:state.numOfCakes-1
-        }
-        case ADD_CAKE:return{
-            ...state,
-            numOfCakes:state.numOfCakes+1
-        }
-        case BUY_ICECREAM:return{
-            ...state,
-            numOfIcecreams:state.numOfIcecreams-1
-        }
-        case ADD_ICECREAM:return{
-            ...state,
-            numOfIcecreams:state.numOfIcecreams+1
-        }
-        default:return state
+const cakeReducer = (state = initialstate, action) => {
+    switch (action.type) {
+        case BUY_CAKE:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        case ADD_CAKE:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + 1
+            }
+        case BUY_ICECREAM:
+            return {
+                ...state,
+                numOfIcecreams: state.numOfIcecreams - 1
+            }
+        case ADD_ICECREAM:
+            return {
+                ...state,
+                numOfIcecreams: state.numOfIcecreams + 1
+            }
+        default:
+            return state
     }
 
 }
